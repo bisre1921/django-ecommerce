6ce67cb3e4a47f68a653c7cd184b72d4da6d73c4
bisre1921 <bisrattewodros3@gmail.com>
2024-11-18 20:42:45 +0300

    implement register new users functionality

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render , redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 # Create your views here.
 def home(request):
@@ -30,3 +34,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You are now logged out")
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(request , username=username , password=password)
+            login(request , user)
+            messages.success(request, "Account was created for " + username)
+            return redirect('home')
+        else:
+            messages.success(request, "Invalid credentials, try again")
+            return redirect('register') 
+    else:
+        return render(request , 'register.html' , {'form': form})
