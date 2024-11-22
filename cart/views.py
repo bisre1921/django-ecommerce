42473c1df53b365e15c73423be3aa0c3cba8a203
bisre1921 <bisrattewodros3@gmail.com>
2024-11-22 22:08:56 +0300

    implement add product to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 , redirect
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
 
 # Create your views here.
 def cart_summary(request):
@@ -6,8 +10,13 @@ def cart_summary(request):
     
 
 def cart_add(request):
-    # return render(request , 'cart_add.html')
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product , id=product_id)
+        cart.add(product=product)
+        response = JsonResponse({'Product Name: ': product.name})
+        return response
 
 def cart_delete(request):
     # return render(request , 'cart_delete.html')
