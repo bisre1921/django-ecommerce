c3aeb1a7ec307e2c3fcd841cae1ab18f50e2d7a2
bisre1921 <bisrattewodros3@gmail.com>
2024-11-24 14:45:20 +0300

    update shopping cart summary page

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,5 @@
+from store.models import Product
+
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -21,4 +23,9 @@ class Cart():
 
     def __len__(self):
         return len(self.cart)
+    
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        return products
         
